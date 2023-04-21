281fceadd4c49817c0c8d59f5609b8617c679a71
vediicaa <100271946+vediicaa@users.noreply.github.com>
2023-04-22 00:57:02 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/vediicaa/MyPortfolio/master/src/Assets/Vedica'sResume.pdf";
+  "C:\Users\vedica\Documents\GitHub\MyPortfolio\src\Assets\Vedica'sResume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
